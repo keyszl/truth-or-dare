af6e93ed3c2d4ed0475ddd736531e987bdb1107c
Ketsia <dusengeketty1@gmail.com>
2022-10-03 19:09:09 -0500

    Added navigations to different pages when an icon button is clicked

--- truth_or_dare/lib/main.dart
+++ truth_or_dare/lib/main.dart
@@ -3,8 +3,5 @@ import 'firstscreen.dart';
 import 'secondscreen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: "Truth/Dare",
-    home: FirstScreen(),
-  ));
+  runApp(const MaterialApp(title: "Truth/Dare", home: FirstScreen()));
 }
