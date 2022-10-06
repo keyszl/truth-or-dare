3935737c430e92372e33fba3a5da21d7d45d1606
mason-pAoLo <masonmillionaire725@gmail.com>
2022-10-06 17:47:27 -0500

    Added Chat Functionality
    You now have a friends list and can tap on one of your friends to chat with them, however there are now issues with navigation that must be fixed.

---
+++ truth_or_dare/lib/chat.dart
@@ -0,0 +1,56 @@
+import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:flutter/material.dart';
+import 'package:truth_or_dare/text_widgets.dart';
+
+import 'friends_data.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key, required this.friend});
+
+  final Friend? friend;
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void initState() {
+    super.initState();
+    widget.friend!.addListener(update);
+  }
+
+  void dispose() {
+    widget.friend!.removeListener(update);
+    print("Goodbye");
+    super.dispose();
+  }
+
+  void update() {
+    print("New message!");
+    setState(() {});
+  }
+
+  Future<void> send(String msg) async {
+    await widget.friend!.send(msg).catchError((e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Error: $e"),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat with " + widget.friend!.name),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Expanded(child: widget.friend!.bubble_history()),
+          MessageBar(onSend: (_) => send(_)),
+        ],
+      ),
+    );
+  }
+}
