be0b71205af20d55d85856bbffa0760b597edeb8
mason-pAoLo <masonmillionaire725@gmail.com>
2022-10-06 17:47:27 -0500

    Added Chat Functionality
    You now have a friends list and can tap on one of your friends to chat with them, however there are now issues with navigation that must be fixed.

--- truth_or_dare/lib/firstscreen.dart
+++ truth_or_dare/lib/firstscreen.dart
@@ -1,8 +1,76 @@
 import 'package:flutter/material.dart';
 import 'secondscreen.dart';
 
-class FirstScreen extends StatelessWidget {
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:network_info_plus/network_info_plus.dart';
+
+import 'package:truth_or_dare/friends_data.dart';
+
+class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  String? _ipaddress = "Loading...";
+  late StreamSubscription<Socket> server_sub;
+  late Friends _friends;
+  late List<DropdownMenuItem<String>> _friendList;
+  late TextEditingController _nameController, _ipController;
+
+  void initState() {
+    super.initState();
+    _friends = Friends();
+    _friends.add("Self", "127.0.0.1");
+    _nameController = TextEditingController();
+    _ipController = TextEditingController();
+    _setupServer();
+    _findIPAddress();
+  }
+
+  void dispose() {
+    server_sub.cancel();
+    super.dispose();
+  }
+
+  Future<void> _findIPAddress() async {
+    // Thank you https://stackoverflow.com/questions/52411168/how-to-get-device-ip-in-dart-flutter
+    String? ip = await NetworkInfo().getWifiIP();
+    setState(() {
+      _ipaddress = "My IP: " + ip!;
+    });
+  }
+
+  Future<void> _setupServer() async {
+    try {
+      ServerSocket server =
+          await ServerSocket.bind(InternetAddress.anyIPv4, ourPort);
+      server_sub = server.listen(_listenToSocket); // StreamSubscription<Socket>
+    } on SocketException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Error: $e"),
+      ));
+    }
+  }
+
+  void _listenToSocket(Socket socket) {
+    socket.listen((data) {
+      setState(() {
+        _handleIncomingMessage(socket.remoteAddress.address, data);
+      });
+    });
+  }
+
+  void _handleIncomingMessage(String ip, Uint8List incomingData) {
+    String received = String.fromCharCodes(incomingData);
+    print("Received '$received' from '$ip'");
+    _friends.receiveFrom(ip, received);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +82,12 @@ class FirstScreen extends StatelessWidget {
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body: const Text("..."),
+      body: Text(_ipaddress!, textAlign: TextAlign.center),
       bottomNavigationBar: Container(
         height: 50,
         decoration: BoxDecoration(
           color: Colors.pink[50],
-          borderRadius: BorderRadius.only(
+          borderRadius: const BorderRadius.only(
             topLeft: Radius.circular(10),
             topRight: Radius.circular(10),
           ),
@@ -28,20 +96,18 @@ class FirstScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             IconButton(
-              key: Key('iconbutton1'),
-              icon: Icon(Icons.home),
+              key: const Key('iconbutton1'),
+              icon: const Icon(Icons.home),
               iconSize: 30,
               onPressed: () {},
             ),
             IconButton(
-              key: Key('iconbutton2'),
-              icon: Icon(Icons.contacts),
+              key: const Key('iconbutton2'),
+              icon: const Icon(Icons.contacts),
               iconSize: 30,
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondScreen()));
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => SecondScreen(friends: _friends)));
                 //Navigator.pop(context);
               },
             )
--- truth_or_dare/lib/secondscreen.dart
+++ truth_or_dare/lib/secondscreen.dart
@@ -1,8 +1,109 @@
 import 'package:flutter/material.dart';
 import 'firstscreen.dart';
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+import 'package:truth_or_dare/friends_data.dart';
+import 'package:truth_or_dare/text_widgets.dart';
+import 'package:truth_or_dare/chat.dart';
+import 'package:truth_or_dare/list_items.dart';
+
+class SecondScreen extends StatefulWidget {
+  SecondScreen({super.key, required this.friends});
+
+  Friends? friends;
+
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  String? _ipaddress = "Loading...";
+  late Friends _friends;
+  late List<DropdownMenuItem<String>> _friendList;
+  late TextEditingController _nameController, _ipController;
+
+  void initState() {
+    super.initState();
+    _friends = widget.friends!;
+    _nameController = TextEditingController();
+    _ipController = TextEditingController();
+  }
+
+  Future<void> _handleChat(Friend friend) async {
+    print("Chat");
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ChatScreen(friend: friend),
+      ),
+    );
+  }
+
+  void _handleEditFriend(Friend friend) {
+    setState(() {
+      print("Edit");
+    });
+  }
+
+  void addNew() {
+    setState(() {
+      _friends.add(_nameController.text, _ipController.text);
+    });
+  }
+
+  final ButtonStyle yesStyle =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+  final ButtonStyle noStyle =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    print("Loading Dialog");
+    _nameController.text = "";
+    _ipController.text = "";
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Add A Friend'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextEntry(
+                    width: 200,
+                    label: "Name",
+                    inType: TextInputType.text,
+                    controller: _nameController),
+                TextEntry(
+                    width: 200,
+                    label: "IP Address",
+                    inType: TextInputType.number,
+                    controller: _ipController),
+              ],
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                key: const Key("CancelButton"),
+                style: noStyle,
+                child: const Text('Cancel'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              ElevatedButton(
+                key: const Key("OKButton"),
+                style: yesStyle,
+                child: const Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    addNew();
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +113,25 @@ class SecondScreen extends StatelessWidget {
         title: const Text("CONTACTS",
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
       ),
-      body: const Text("..."),
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          children: _friends.map((name) {
+            return FriendListItem(
+              friend: _friends.getFriend(name)!,
+              onListTapped: _handleChat,
+              onListEdited: _handleEditFriend,
+            );
+          }).toList(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _displayTextInputDialog(context);
+        },
+        tooltip: 'Add Friend',
+        child: const Icon(Icons.add),
+      ),
       bottomNavigationBar: Container(
         height: 50,
         decoration: BoxDecoration(
@@ -30,10 +149,8 @@ class SecondScreen extends StatelessWidget {
               iconSize: 30,
               icon: Icon(Icons.home),
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FirstScreen()));
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const FirstScreen()));
               },
             ),
             IconButton(
