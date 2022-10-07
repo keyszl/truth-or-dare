d36693880a0fcbc83203b96a4e5bd32a06391d2d
keyszl <keyszl@hendrix.edu>
2022-10-07 11:05:14 -0500

    created skeleton of truth/dare button

--- truth_or_dare/lib/firstscreen.dart
+++ truth_or_dare/lib/firstscreen.dart
@@ -76,13 +76,63 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue[200],
+        backgroundColor: Colors.lightBlue[190],
         title: const Text(
           "HOME",
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Text(_ipaddress!, textAlign: TextAlign.center),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //const SizedBox(height: 30),
+          SizedBox(
+            height: 70,
+            width: 180,
+            child: TextButton(
+              // TRUTH BUTTON
+              key: const Key("Truth Button"),
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 175, 219, 237),
+                padding: const EdgeInsets.all(16.0),
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                // do something
+              },
+              child: const Text('TRUTH'),
+            ),
+          ),
+          const SizedBox(height: 30),
+          SizedBox(
+            height: 70,
+            width: 180,
+            child: TextButton(
+              //DARE BUTTON
+              key: const Key("Dare Button"),
+              style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 175, 219, 237),
+                padding: const EdgeInsets.all(16.0),
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {
+                // do something
+              },
+              child: const Text('DARE'),
+            ),
+          )
+        ],
+      )),
+      //Text(_ipaddress!, textAlign: TextAlign.center),
       bottomNavigationBar: Container(
         height: 50,
         decoration: BoxDecoration(
