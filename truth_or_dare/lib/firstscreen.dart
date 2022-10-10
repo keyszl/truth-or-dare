a652c490fb23e37d177532f06b4e6a49cf8a8d74
keyszl <keyszl@hendrix.edu>
2022-10-09 20:11:15 -0500

    creating dialog box and dares/truth files

--- truth_or_dare/lib/firstscreen.dart
+++ truth_or_dare/lib/firstscreen.dart
@@ -1,6 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:convert';
+
+import 'package:network_info_plus/network_info_plus.dart';
+
 import 'package:truth_or_dare/friends_data.dart';
+import 'globals.dart' as globals;
+import 'package:path/path.dart' as p;
 
 class TruthDareScreen extends StatefulWidget {
   TruthDareScreen({super.key, required this.friends});
@@ -16,6 +27,54 @@ class _TruthDareScreenState extends State<TruthDareScreen> {
 
   void initState() {
     _friends = widget.friends!;
+  }
+
+  //https://api.flutter.dev/flutter/dart-io/File-class.html - how files work
+  void _getPrompt(String promptType) async {
+    var filePath = p.join(Directory.current.path, 'dares.txt');
+
+    if (promptType == "truth") {
+      filePath = p.join(Directory.current.path, 'lib', 'truths.txt');
+    }
+
+    File file = File(filePath);
+
+    Stream<String> lines = file
+        .openRead()
+        .transform(utf8.decoder) // Decode bytes to UTF-8.
+        .transform(LineSplitter()); // Convert stream to individual lines.
+
+    Random random = new Random();
+    int length = 0;
+    await for (var line in lines) {
+      length += 1;
+    }
+
+    int rint = random.nextInt(length);
+    print(rint);
+    globals.promptText = lines.elementAt(rint) as String;
+  }
+
+  Widget _buildPopupDialog(BuildContext context) {
+    return new AlertDialog(
+      title: const Text('Prompt:'),
+      //mainAxisSize: MainAxisSize.min,
+      content: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(globals.promptText),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Close'),
+        ),
+      ],
+    );
   }
 
   @override
@@ -42,6 +101,11 @@ class _TruthDareScreenState extends State<TruthDareScreen> {
             ),
             onPressed: () {
               // do something
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      _buildPopupDialog(context));
+              _getPrompt("truth");
             },
             child: const Text('TRUTH'),
           ),
@@ -64,6 +128,11 @@ class _TruthDareScreenState extends State<TruthDareScreen> {
             ),
             onPressed: () {
               // do something
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) =>
+                      _buildPopupDialog(context));
+              _getPrompt("truth");
             },
             child: const Text('DARE'),
           ),
